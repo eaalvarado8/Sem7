2c1602b038f1b4fdcf28de0696eecece05f503c9
eaalvarado8 <eric_alvarado2202@gmail.com>
2022-11-23 17:21:21 -0500

    Up

---
+++ app/src/main/java/com/example/sem7/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.sem7
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_acceso.setOnClickListener(){
+            var saltarGPS:Intent= Intent(this, login::class.java)
+            startActivity(saltarGPS)
+        }
+    }
+}
